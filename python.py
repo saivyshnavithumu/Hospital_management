b2fb2ae7a9e3a3ddcc48c7b39122580a4c003065
saivyshnavithumu <saivyshnavi1903@gmail.com>
2023-11-17 06:15:39 +0530

    created a nerw file python.py

---
+++ python.py
@@ -0,0 +1,27 @@
+'''
+n=(input("enter the name:"))
+consonants=0
+vowels=0
+for i in n:
+    if i in ('A','E','I','O','U' and 'a','e','i','o','u'):
+        vowels=vowels+1
+    else:
+        consonants=consonants+1  
+print("number of consonants",consonants)
+print("number of vowels",vowels)
+'''
+
+
+'''
+n=int(input("enter the number :"))
+if n<1:
+    print("number is not prime")
+for i in range(2,n):
+   if n%i==0:
+    print("number is not prime")
+    break
+else:
+    print("number is prime")
+'''
+
+
